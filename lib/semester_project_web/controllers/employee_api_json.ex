5f8961e506cda50cffd9df4be73a173c570b78c1
Michał Obara <mobaradev@yahoo.com>
2023-05-04 16:31:47 -0700

    update 1.7.2 and name to first_name update

--- lib/semester_project_web/controllers/employee_api_json.ex
+++ lib/semester_project_web/controllers/employee_api_json.ex
@@ -4,7 +4,7 @@ defmodule SemesterProjectWeb.EmployeeApiJSON do
     %{data:
       for(employee <- employees) do
         %{employee_id: employee.employee_id,
-        name: employee.first_name,
+        first_name: employee.first_name,
         last_name: employee.last_name,
         date_of_hire: employee.date_of_hire,
         department: employee.department}
